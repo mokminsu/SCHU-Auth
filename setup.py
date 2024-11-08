214f3e34c39607c03237f4b4953d31c546ed1002
Minsu Mok <contact@mokminsu.dev>
2024-11-08 15:39:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='mokminsu',
     author_email='content@mokminsu.dev',
     description='Soonchunhyang University Authentication.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/mokminsu/SCHU-Auth',
     packages=find_packages(),
